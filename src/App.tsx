0fc870fa26c4c2eb5cb00c36e4f6cc3d7e766aa2
TheBlankness <iddinishak@gmail.com>
2025-07-21 16:52:21 +0800

    simple pdf test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import { Tldraw } from 'tldraw'
+import { useState } from "react";
+import "tldraw/tldraw.css";
+import { PdfEditor } from "./PdfEditor";
+import { Pdf, PdfPicker } from "./PdfPicker";
+import "./pdf-editor.css";
 
-function App() {
-	return (
-		<div style={{ position: 'fixed', inset: 0 }}>
-			<Tldraw />
-		</div>
-	)
+type State =
+  | {
+      phase: "pick";
+    }
+  | {
+      phase: "edit";
+      pdf: Pdf;
+    };
+
+export default function App() {
+  const [state, setState] = useState<State>({ phase: "pick" });
+
+  switch (state.phase) {
+    case "pick":
+      return (
+        <div className="PdfEditor">
+          <PdfPicker onOpenPdf={(pdf) => setState({ phase: "edit", pdf })} />
+        </div>
+      );
+    case "edit":
+      return (
+        <div className="PdfEditor">
+          <PdfEditor pdf={state.pdf} />
+        </div>
+      );
+  }
 }
-
-export default App
